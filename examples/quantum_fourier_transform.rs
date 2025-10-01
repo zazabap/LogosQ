1574591d4f6c3013a04c5bd0e221c7a0e137bfd9
zazabap <sweynan@icloud.com>
2025-10-01 17:47:55 +0900

    add qft example

--- examples/quantum_fourier_transform.rs
+++ examples/quantum_fourier_transform.rs
@@ -2,6 +2,8 @@ use logosq::algorithms::qft;
 use logosq::circuits::Circuit;
 use logosq::vis::circuit_text;
 use logosq::vis::save_circuit_svg;
+use logosq::vis::Visualizable;
+use logosq::State;
 
 fn main() {
     let num_qubits = 5; // Example with 5 qubits
@@ -10,17 +12,26 @@ fn main() {
 }
 // how to combine algorithms
 pub fn quantum_fourier_transform_example(num_qubits: usize) -> Vec<usize> {
+
+    let mut state = State::zero_state(num_qubits);
     // Create a circuit that demonstrates QFT
     let mut circuit = Circuit::new(num_qubits);
-
-    // Put qubits into superposition
-    for i in 0..num_qubits {
-        circuit.h(i);
-    }
+    circuit.x(0);
+    circuit.execute(&mut state);
 
     // Apply QFT
-    let qft_circuit = qft::create_circuit(num_qubits);
-    circuit.compose(&qft_circuit);
+    qft::apply(&mut state);
+    print!("State after QFT: {}\n", state.visualize());
+    qft::apply_inverse(&mut state);
+    print!("State after inverse QFT: {}\n", state.visualize());
+    circuit.execute_and_measure()
+}
+
+
+pub fn qft_c2 (){
+    let num_qubits = 3; // Example with 3 qubits
+    let circuit = Circuit::new(num_qubits);
+    // circuit.compose(&qft::create_circuit(num_qubits));
 
     print!("\nCircuit diagram:\n");
     print!("{}", circuit_text(&circuit));
@@ -29,5 +40,5 @@ pub fn quantum_fourier_transform_example(num_qubits: usize) -> Vec<usize> {
     });
 
     // Measure all qubits
-    circuit.execute_and_measure()
+    circuit.execute_and_measure();
 }
